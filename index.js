583399db5db5323a01dab164730f85c20678184b
GLADYS <mungaiglado106@gmail.com>
2022-06-10 16:14:53 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'some intial value'
+var bestCustomer; 
+
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'billy'
+}
